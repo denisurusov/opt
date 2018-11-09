016f834103aaba1e4a601b8a6bfb9b0e209dc5f3
denisurusov <denis.urusov@gmail.com>
2018-11-09 02:47:01 +0000

    cleanup

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import data.model.Direction;
+import data.model.Parameter;
+
 
 public class Main {
 
     public static void main(String[] args) {
 	    // write your code here
+	    Parameter hoursADay = new Parameter("Number of work hours a day", Direction.UP, 8);
+	    
 	    System.out.println("Hello World Denis!2");
     }
 }
